c6ce6f42840021cfa4e6fe301b529afcfed944fa
JMmandoo <leejeamon1994@gamil.com>
2023-12-09 18:06:36 +0900

    fit: camera.js, 기타 모든 html 수정 - camera.js 노트북 카메라를 통해 사진 찰영후 결과 페이지로 이미지가 넘어 갈수 있도록 수정 - 사진촬영하는 html과 결과 html코드 수정 - html을 연결시켜주는 urls.py, views.py 수정

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from mainapp.views import main, personalcolor_view, style_view, hairloss_view
+from mainapp.views import main, personalcolor_view, style_view, hairloss_view, hairlossresult_view,personalcolorresult_view,styleresult_view
 
 app_name = 'mainapp'
 
@@ -9,4 +9,7 @@ urlpatterns = [
     path('personalcolor_view/', personalcolor_view, name='personalcolor_view'),
     path('style_view/', style_view, name='style_view'),
     path('hairloss_view/', hairloss_view, name='hairloss_view'),
+    path('hairlossresult_view/', hairlossresult_view, name='hairlossresult_view'),
+    path('personalcolorresult_view/', personalcolorresult_view, name='personalcolorresult_view'),
+    path('styleresult_view/', styleresult_view, name='styleresult_view'),
 ]
